2a9eb0f69ef82f2ced5c29d362dad770a04d8d30
JhoanMonsalve <jhoanmonsalve320@gmail.com>
2024-11-18 10:36:15 -0500

    registro subido

---
+++ src/public/js/login.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("login-form");
+    const registerForm = document.getElementById("register-form");
+    const showLogin = document.getElementById("show-login");
+    const showRegister = document.getElementById("show-register");
+  
+    // Mostrar formulario de registro
+    showRegister.addEventListener("click", () => {
+      loginForm.classList.add("hidden");
+      registerForm.classList.remove("hidden");
+    });
+  
+    // Mostrar formulario de login
+    showLogin.addEventListener("click", () => {
+      registerForm.classList.add("hidden");
+      loginForm.classList.remove("hidden");
+    });
+  });
+  
